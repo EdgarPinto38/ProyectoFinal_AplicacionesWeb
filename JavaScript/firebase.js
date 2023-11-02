e44b646b11be35dd2de412b9055bc5923478977c
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-11-02 00:50:43 -0600

    agregando nav mar

---
+++ JavaScript/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCtlqfUWhACEtMzFdxtHSRjPbgpIrzkWtI",
+    authDomain: "tiendaplantas-cf8bf.firebaseapp.com",
+    projectId: "tiendaplantas-cf8bf",
+    storageBucket: "tiendaplantas-cf8bf.appspot.com",
+    messagingSenderId: "613046897699",
+    appId: "1:613046897699:web:cf02b294bacca1dad53596"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
