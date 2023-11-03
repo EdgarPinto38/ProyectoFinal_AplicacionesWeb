933ae334fbfdb2ebe17dbfdd433f817846f6fdae
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-11-02 21:35:16 -0600

    agregando registro de usuario a firebase

--- JavaScript/signupForm.js
+++ JavaScript/signupForm.js
@@ -1,9 +1,24 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
+import { auth } from './firebase.js'
 const signupForm = document.querySelector('#signup-form')
 console.log(signupForm)
-document.getElementById('signup-form').addEventListener('submit',function(event) {
+document.getElementById('signup-form').addEventListener('submit', async function (event) {
     event.preventDefault();
     const email = signupForm['signup-email'].value
     const password = signupForm['signup-password'].value
+    console.log(email, password);
 
-    console.log(email, password)
+    try {
+
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(userCredentials);
+        //cerrar el modal cuando se registra un usuario
+        const signupModal = document.querySelector('#signupModal')
+        const modal = bootstrap.Modal.getInstance(signupModal)
+        modal.hide()
+    } catch (error) {
+        console.log(error);
+    }
+
+
 })
