997c819ba56342cc022c1e5508c6c9587fd34046
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-11-09 00:18:11 -0600

    agregando recuperacion de password

---
+++ JavaScript/forgotPassword.js
@@ -0,0 +1,16 @@
+import { sendPasswordResetEmail }  from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
+import { auth } from '../JavaScript/firebase.js'
+import { showMessage } from '../JavaScript/showMessage.js'
+
+const forgotF = document.querySelector('#forgot-form')
+console.log(forgotF)
+document.getElementById('forgot-form').addEventListener('submit',async function(e){
+    e.preventDefault()
+    const email = forgotF['forgotEmail'].value;
+    sendPasswordResetEmail(auth, email)
+    try {
+        console.log(email)
+    } catch (error) {
+        showMessage("Something went wrong", "error")
+    }
+})
